0b5bc2dc6b7afc210b1ccff79b424d771ebca64a
aghuddleston <aghuddleston@users.noreply.github.com>
2015-02-16 09:30:26 -0500

    cdn address change

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var Harness = Siesta.Harness.Browser.ExtJS;
 Harness.configure({
     title       : 'Ext.ux.data.PagingStore Test Suite',
     preload     : [
-	    { text : 'console.log("begin main preloading");' },
-      "http://cdn.sencha.io/ext-4.1.0-gpl/ext-all-debug.js",
-		  './Ext.ux.data.PagingStore.js',
-	    { text : 'console.log("finishing preloading");' }
-    ]
+		{ text : 'console.log("begin main preloading");' },
+		"http://extjs.cachefly.net/ext-4.1.0-gpl/ext-all.js",
+		'./Ext.ux.data.PagingStore.js',
+		{ text : 'console.log("finishing preloading");' }
+	]
 });
 
 Harness.start(
   '01_ArrayPagingStoreTest.js',
-	'02_JsonPagingStoreTest.js'
+  '02_JsonPagingStoreTest.js'
 );
